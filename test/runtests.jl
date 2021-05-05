cc0413f81415385060471fb047c29e9b8f21d5a1
Irene <irene.ndindabahizi@gmail.com>
2021-05-05 11:30:14 +0200

    add ExternalBallistics repo
    switch from local repo to github

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,12 +3,12 @@ using Test
 using ExternalBallistics
 
 
-drone = createTargetRect(15.0,25.0,800.0)
-ϵ = RectErrorB(25.0,100.0,3.0,-10.0)
 
 
 @testset "Cuas.jl" begin
     # Write your tests here.
+    drone = createTargetRect(15.0,25.0,800.0)
+    ϵ = RectErrorB(25.0,100.0,3.0,-10.0)
     @test pHit(drone,ϵ)== 0.0881314035058921
 
 end
